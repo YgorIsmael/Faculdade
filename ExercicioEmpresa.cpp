6870590be7a8211510ab86d5d7b7676bda327d42
Ygor Ismael <74953869+YgorIsmael@users.noreply.github.com>
2023-04-08 15:26:29 -0300

    Add files via upload

---
+++ ExercicioEmpresa.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int tempo, i, y, maiortempo, menortempo;
+	char cargo;
+	bool flag;
+	float salario, media, remuneracao, menorremuneracao;
+	flag = true;
+	i = 1;
+	y = 0;
+	media = 0;
+	maiortempo = 0;
+	menortempo = 10000;
+	while (i <= 5) {
+	cout << "Insira os seguintes dados respectivamente:\nTempo na empresa\nCargo (V-vendedor, C-chefe, E-estoque)\nSalario\n";
+	cin >> tempo >> cargo >> salario;
+		if (tempo >= maiortempo) {
+			maiortempo = tempo;
+		}
+		if (cargo == 'V' || cargo == 'v'){
+			remuneracao = salario + ((0.005 * salario) * tempo);
+			cout << "Remuneracao:\n" << remuneracao << "\n";
+			media = media + remuneracao;
+			y++;
+		}
+		else if (cargo == 'C' || cargo == 'c'){
+			remuneracao = salario + (2000 * tempo);
+			cout << "Remuneracao:\n" << remuneracao << "\n";
+			if (tempo < 10) {
+				flag = false;
+			}
+		}
+		else if (cargo == 'E' || cargo == 'e') {
+			if (tempo >= 5) {
+				tempo = tempo / 5;
+				remuneracao = salario + (0.1 * salario * tempo);
+				cout << "Remuneracao:\n" << remuneracao << "\n";
+			}
+			else {
+				cout << "Remuneracao:\n" << remuneracao << "\n";
+			}
+			
+			if (tempo <= menortempo) {
+			menortempo = tempo;
+			menorremuneracao = remuneracao;
+			}
+		}
+		else {
+			cout << "Letra invalida!\n";
+		}
+		i++;
+}	
+    cout << "Maior tempo na empresa:\n" << maiortempo << "\n";
+    cout << "Média de remuneração dos vendedores:\n" << media / y << "\n";
+    cout << "Menor remuneracao de um funcionario de estoque:\n" << menorremuneracao << "\n";
+    
+    if (flag == false){
+    	cout << "Nao sao todos os chefes com mais de 10 anos na empresa.\n";
+	}
+	else {
+		cout << "Todos os chefes tem mais de 10 anos na empresa.\n";
+	}
+}
