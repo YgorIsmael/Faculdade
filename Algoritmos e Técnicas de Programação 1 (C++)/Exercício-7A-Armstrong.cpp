484343080f2a81888dae392cbc0774bd936eec58
Ygor Ismael <74953869+YgorIsmael@users.noreply.github.com>
2023-05-28 16:59:46 -0300

    Update Exercício-7A-Armstrong.cpp

--- Algoritmos e Técnicas de Programação 1 (C++)/Exercício-7A-Armstrong.cpp
+++ Algoritmos e Técnicas de Programação 1 (C++)/Exercício-7A-Armstrong.cpp
@@ -1,6 +1,6 @@
-//Número de Armstrong é um número de n dígitos que é igual a soma de cada um dos seus dígitos elevado a n-ésima potência.
-//Por exemplo, 153 (n = três dígitos) é igual a 13 + 53 + 33 = 1 + 125 + 27 = 153.
-//Crie um algoritmo que determine se um número é número de Armstrong.
+//NÃºmero de Armstrong Ã© um nÃºmero de n dÃ­gitos que Ã© igual a soma de cada um dos seus dÃ­gitos elevado a n-Ã©sima potÃªncia.
+//Por exemplo, 153 (n = trÃªs dÃ­gitos) Ã© igual a 13 + 53 + 33 = 1 + 125 + 27 = 153.
+//Crie um algoritmo que determine se um nÃºmero Ã© nÃºmero de Armstrong valendo atÃ© n = 999.
 
 #include <iostream>
 using namespace std;
